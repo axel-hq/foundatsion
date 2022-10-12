0f1ab0e1fbc7383765291fb0f4cd3098b5d2c653
AS-Pando <106735646+AS-Pando@users.noreply.github.com>
2022-10-11 17:58:03 -0700

    solved the lines problem

--- src/err.ts
+++ src/err.ts
@@ -1,16 +1,74 @@
+const x = typeof window;
+type all = typeof x;
+
+type einp = string | TSFoundationError | Error;
+
+type lent = string | lent[];
+
 export class TSFoundationError extends Error {
-   lines: string[];
+   lines: lent[];
    /**
     * Use `"\n"` to indicate a separate line.
     */
-   constructor (...msg: unknown[]) {
-      const forced_lines: string[] = [];
-      for (const e of msg) {
-         if (e instanceof TSFoundationError) {
-            forced_lines.push(...e.lines);
+   constructor (...msg: einp[]) {
+      const thisdotlines: lent[] = [];
+      let linebuffer: string | null = null;
+      for (let e of msg) {
+         if (typeof e === "string") {
+            for (
+               // iterate through lines
+               let idx: number;
+               (idx = e.indexOf("\n")) >= 0;
+               e = e.slice(idx + 1))
+            {
+               const line = e.slice(0, idx);
+               if (linebuffer === null) {}
+               else {
+                  thisdotlines.push(linebuffer);
+               }
+               linebuffer = line;
+            }
+            // from here on out, e doesn't contain any newline
+            // there might not be anything left in e if it ended with a newline
+            if (e.length > 0) {
+               if (linebuffer === null) {
+                  linebuffer = e;
+               } else {
+                  linebuffer += ` ${e}`;
+               }
+            }
             continue;
          }
-         this.lines[this.lines.length - 1] += ` ${line}`;
+         if (e instanceof TSFoundationError) {
+            if (linebuffer === null) {}
+            else {
+               thisdotlines.push(linebuffer);
+            }
+            thisdotlines.push(e.lines);
+            continue;
+         }
+         if (e instanceof Error) {
+            if (linebuffer === null) {}
+            else {
+               thisdotlines.push(linebuffer);
+            }
+            const sublines = [e.name];
+            let msg = e.message;
+            for (
+               // iterate through lines in e.message
+               let idx: number;
+               (idx = msg.indexOf("\n")) >= 0;
+               msg = msg.slice(idx + 1))
+            {
+               sublines.push(msg.slice(0, idx));
+            }
+            // now msg has no more newlines in it
+            // but it might also have nothing in it
+            if (msg.length > 0) {
+               sublines.push(msg);
+            }
+            thisdotlines.push(sublines);
+         }
       }
       super(lines.join("\n"));
    }
@@ -18,8 +76,7 @@ export class TSFoundationError extends Error {
 
 new TSFoundationError(
    "this is on the first line",
-   "this is still on the first line",
-   "\n",
+   "this is still on the first line\n",
    "this is on the second line",
    "this i"
 )
