f97d83ee3f1b214986bf8de1562242a1fe218684
AS-Pando <106735646+AS-Pando@users.noreply.github.com>
2022-10-20 19:22:08 -0700

    I meant copied it from boolean

---
+++ src/symbol.ts
@@ -0,0 +1,18 @@
+import {FoundatsionError} from "./err";
+
+export namespace symbol {
+   export const name = "symbol";
+
+   export function is(u: unknown): u is symbol {
+      return typeof u === "symbol";
+   }
+
+   export function assert(u: unknown): asserts u is symbol {
+      if (typeof u !== "symbol") {
+         throw new FoundatsionError(
+            "Tried asserting for symbol but failed.\n",
+            `typeof value was "${typeof u}" when it should've been "symbol".`,
+         );
+      }
+   }
+}
