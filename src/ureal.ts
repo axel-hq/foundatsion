38d27d2f337a03f0f244f266e53d89ec459c532f
AS-Pando <106735646+AS-Pando@users.noreply.github.com>
2022-11-14 10:44:53 -0800

    morning patch v0.4.1

--- src/ureal.ts
+++ src/ureal.ts
@@ -1,7 +1,6 @@
-import {int} from "./int";
 import {real} from "./real";
 import {inter} from "./inter";
 import {unsigned} from "./unsigned";
 
-export type ureal = unsigned & int;
+export type ureal = unsigned & real;
 export const ureal = inter(unsigned, real);
